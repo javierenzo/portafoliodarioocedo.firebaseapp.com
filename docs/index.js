1c6572adf63f3243ab58239fb75f46b26cca81da
enzogit config --global user.mail darioenzojavier@gmail.comgit config --list --show--origin$ git config --global user.name enzo <darioenzojavier@gmail.com>
2021-02-08 17:28:58 -0300

    no message

--- docs/index.js
+++ docs/index.js
@@ -4,49 +4,49 @@ const menu = document.querySelector(".navbar__menu");
 const links = document.querySelector(".navbar__links");
 const sections = document.querySelectorAll("section");
 const config = {
-  rootMargin: "0px",
-  threshold: [0.6, 0.9],
+    rootMargin: "0px",
+    threshold: [0.6, 0.9],
 };
 
 function handleLlinks() {
-  if (window.innerWidth <= 991) {
-    links.classList.toggle("visible");
-  }
+    if (window.innerWidth <= 991) {
+        links.classList.toggle("visible");
+    }
 }
 
 menu.addEventListener("click", handleLlinks);
 links.addEventListener("click", handleLlinks);
 
-window.addEventListener("scroll", function () {
-  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
-  window.scrollY < 100 && (navbar.style.background = `transparent`);
+window.addEventListener("scroll", function() {
+    window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
+    window.scrollY < 100 && (navbar.style.background = `transparent`);
 });
 
-let observer = new IntersectionObserver(function (entries, self) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      intersectionHandler(entry);
-    }
-  });
+let observer = new IntersectionObserver(function(entries, self) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            intersectionHandler(entry);
+        }
+    });
 }, config);
 
 sections.forEach((section) => {
-  observer.observe(section);
+    observer.observe(section);
 });
 
 function intersectionHandler(entry) {
-  const id = entry.target.id;
-  const currentlyActive = document.querySelector(".navbar__links  .active");
-  const shouldBeActive = document.querySelector(
-    ".navbar__links [data-ref=" + id + "]"
-  );
+    const id = entry.target.id;
+    const currentlyActive = document.querySelector(".navbar__links  .active");
+    const shouldBeActive = document.querySelector(
+        ".navbar__links [data-ref=" + id + "]"
+    );
 
-  if (currentlyActive) {
-    currentlyActive.classList.remove("active");
-  }
-  if (shouldBeActive) {
-    shouldBeActive.classList.add("active");
-  }
+    if (currentlyActive) {
+        currentlyActive.classList.remove("active");
+    }
+    if (shouldBeActive) {
+        shouldBeActive.classList.add("active");
+    }
 }
 
 // window.addEventListener("mousemove", parallax);
@@ -74,6 +74,6 @@ ScrollReveal().reveal(".button--cta", { delay: 550 });
 ScrollReveal().reveal(".skill__title", { delay: 450 });
 ScrollReveal().reveal(".skill__item", { delay: 450 });
 ScrollReveal().reveal(".services__item", { delay: 450 });
-ScrollReveal().reveal(".portfolio__item", { delay: 450 });
+ScrollReveal().reveal(".portafolio__item", { delay: 450 });
 ScrollReveal().reveal(".contact__item", { delay: 450 });
 ScrollReveal().reveal(".footer", { delay: 450 });
